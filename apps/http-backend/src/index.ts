272cc5104c725d492b6b38b7aae8ef925a9e905d
xnki7 <codeinja7@gmail.com>
2025-07-24 16:33:25 +0530

    Zod schema validation

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
 import { middleware } from './middleware';
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from '@repo/common/types';
 
 const app = express();
 
 app.post("/signup", (req, res) => {
     // Simulate user signup logic
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        return res.status(400).json({
+            message: "Invalid input",
+        });
+    }
     console.log("User signed up");
     res.status(201).send("User signed up successfully");
 }
@@ -14,6 +20,12 @@ app.post("/signup", (req, res) => {
 
 app.post("/signin", (req, res) => {
     // Simulate user signin logic
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success) {
+        return res.status(400).json({
+            message: "Invalid input",
+        });
+    }
     console.log("User signed in");
     res.status(200).send("User signed in successfully");
 }
@@ -21,7 +33,12 @@ app.post("/signin", (req, res) => {
 
 app.post("/room", middleware, (req, res) => {
     // Room logic
-    // db logic
+    const data = CreateRoomSchema.safeParse(req.body);
+    if (!data.success) {
+        return res.status(400).json({
+            message: "Invalid input",
+        });
+    }
     res.json({
         roomId: 123
     })
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from './config';
+import { JWT_SECRET } from '@repo/backend-common/config';
 
 export function middleware(req: Request, res: Response, next: NextFunction) {
     const token = req.headers["authorization"] ?? "";
